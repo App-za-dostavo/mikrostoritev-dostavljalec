8bb796e7b35eccd333165ee852a7c63b813c8a13
ts22842 <ts22842@student.uni-lj.si>
2022-12-04 15:23:28 +0100

    Added k8s deployment

--- api/src/main/java/si/fri/rso/dostavljalec/api/v1/DeliveryPersonApplication.java
+++ api/src/main/java/si/fri/rso/dostavljalec/api/v1/DeliveryPersonApplication.java
@@ -9,7 +9,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @OpenAPIDefinition(info = @Info(title = "Dostavljalec API", version = "v1",
-        license = @License(name = "dev"), description = "API for managing image metadata."),
+        license = @License(name = "dev")),
         servers = @Server(url = "http://localhost:8080/"))
 @ApplicationPath("/v1")
 public class DeliveryPersonApplication extends Application {
