4b1717c3c0b62912cecd195daefcea9aac55de9a
ts22842 <ts22842@student.uni-lj.si>
2023-01-09 19:52:37 +0100

    Correct port numbers

--- models/src/main/resources/sql-scripts/init-db.sql
+++ models/src/main/resources/sql-scripts/init-db.sql
@@ -1,5 +1,6 @@
 INSERT INTO dostavljalec (firstName, lastName, meansOfTransport, availability, latitude, longitude) VALUES ('Andrej', 'Hrovat', 'car', true, 46.043339, 14.504097);
-INSERT INTO dostavljalec (firstName, lastName, meansOfTransport, availability, latitude, longitude) VALUES ('Luka', 'Novak', 'car', false, 46.048353, 14.499680);
+INSERT INTO dostavljalec (firstName, lastName, meansOfTransport, availability, latitude, longitude) VALUES ('Luka', 'Novak', 'car', true, 46.048353, 14.499680);
 INSERT INTO dostavljalec (firstName, lastName, meansOfTransport, availability, latitude, longitude) VALUES ('Peter', 'Mlakar', 'bike', true, 46.050599, 14.503780);
 INSERT INTO dostavljalec (firstName, lastName, meansOfTransport, availability, latitude, longitude) VALUES ('Eva', 'Golob', 'car', true, 46.051675, 14.510467);
-INSERT INTO dostavljalec (firstName, lastName, meansOfTransport, availability, latitude, longitude) VALUES ('Anja', 'Kotnik', 'bike', false, 46.051675, 14.510467);
+INSERT INTO dostavljalec (firstName, lastName, meansOfTransport, availability, latitude, longitude) VALUES ('Anja', 'Kotnik', 'bike', true, 46.051675, 14.510467);
+INSERT INTO dostavljalec (firstName, lastName, meansOfTransport, availability, latitude, longitude) VALUES ('Jaka', 'Brnot', 'bike', false, 46.0516, 14.503);
